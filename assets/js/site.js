bb3a203c42d52785c377768eb8a674c9f5270a28
Bo Nicolajsen <>
2024-06-19 09:50:12 +0200

    consent

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,39 @@
 // write cool JS hwere!!
+
+
+/* cookie consent
+skal bruge to css classes: cookieBlocker og cookieBlockerIn
+toggler in class, så der kan animeres, ved hjælp af transition
+to buttons til interaktion*/
+
+let cookieBlockElement = document.getElementById('cookieBlock') // blocker element
+
+
+let okButton = document.getElementById('cookieOk')
+
+// til consent
+okButton.addEventListener('click', () => {
+
+    cookieBlockElement.classList.toggle('cookieBlockerIn')
+})
+
+
+// til no consent
+let noButton = document.getElementById('cookieNotOk')
+
+noButton.addEventListener('click', () => {
+
+    window.location.assign('https://en.wikipedia.org/wiki/Cookie_Monster') // går til en ny site.. i assign giver browseren lov til at huske history ikke ..replace
+
+})
+
+
+
+
+
+window.addEventListener('load', () => {
+    console.log('loaded');
+    cookieBlockElement.classList.toggle('cookieBlockerIn')
+})
+
+//---------------------------------------------------------------------------------------------------
